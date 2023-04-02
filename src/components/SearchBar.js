2545acd7c7f9e4adb1e43feb1d9f3bde41fdee63
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-03 00:29:50 +0530

    Used a alternative event handler method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,16 +5,17 @@ import "semantic-ui-css/semantic.min.css";
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value)
-    }
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
 
     render() {
         return (
             <div className='ui segment' >
                 <form className='ui form'>
                     <div className="ui icon input field" id="search-input">
-                        <input type="text" name="seachQuery" placeholder="Search Image..." onChange={this.onInputChange} />
+                        {/* Used a alternative event handler on the line below */}
+                        <input type="text" name="seachQuery" placeholder="Search Image..." onChange={(x) => console.log(x.target.value)} />
                         <i className="circular search link icon" />
                     </div>
                 </form>
