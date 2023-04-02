b53c50eb802a954f55e87225ee323807e7d66715
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-02 23:12:07 +0530

    Added a event handler in the search input field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,17 +3,24 @@ import "./../styles/SearchBar.css";
 import "semantic-ui-css/semantic.min.css";
 
 
-const SearchBar = () => {
-    return (
-        <div className='ui segment'>
-            <form className='ui form'>
-                <div className="ui icon input field" id="search-input">
-                    <input type="text" name="seachQuery" placeholder="Search Image..." />
-                    <i className="circular search link icon" />
-                </div>
-            </form>
-        </div>
-    )
+class SearchBar extends React.Component {
+
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <div className='ui segment' >
+                <form className='ui form'>
+                    <div className="ui icon input field" id="search-input">
+                        <input type="text" name="seachQuery" placeholder="Search Image..." onChange={this.onInputChange} />
+                        <i className="circular search link icon" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SearchBar
