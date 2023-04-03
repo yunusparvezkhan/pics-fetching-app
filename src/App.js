95753de03a4c31f57dca0650de8430d36701a946
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-04 00:04:19 +0530

    Implemented unsplash api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import "semantic-ui-css/semantic.min.css";
+
 
 class App extends React.Component {
 
 
   seachFunc(input) {
-    console.log(input);
+    // console.log(input);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: input },
+      header: {
+        Authorization: 'Clint-ID PLiLgVUo7Z-p2Rkguvg0HYTXUEfu7ORa-5w4lFl1OOo',
+      }
+    });
   };
 
 
