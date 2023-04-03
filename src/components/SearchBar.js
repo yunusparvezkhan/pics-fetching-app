af357bdd68664be94f9fadfb3045b4c55d75d319
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-03 19:28:50 +0530

    Made the input a Controlled Element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,18 +4,18 @@ import "semantic-ui-css/semantic.min.css";
 
 
 class SearchBar extends React.Component {
+    state = { term: "" };
 
-    // onInputChange(event) {
-    //     console.log(event.target.value)
-    // }
+    componentDidUpdate() {
+        console.log(this.state.term)
+    }
 
     render() {
         return (
             <div className='ui segment' >
                 <form className='ui form'>
                     <div className="ui icon input field" id="search-input">
-                        {/* Used a alternative event handler on the line below */}
-                        <input type="text" name="seachQuery" placeholder="Search Image..." onChange={(x) => console.log(x.target.value)} />
+                        <input type="text" name="seachQuery" placeholder="Search Image..." onChange={(e) => this.setState({ term: e.target.value })} />
                         <i className="circular search link icon" />
                     </div>
                 </form>
