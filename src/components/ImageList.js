cc1f90408a07e11b1ed7ed2c676b60a74bbf725c
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-06 23:57:12 +0530

    Created grid view on the ImageList Component using only css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../styles/ImageList.css";
 
 const ImageList = (props) => {
     const results = props.images.map(({ id, description, urls }) => {
-        return <img key={id} alt={description} src={urls.regular} />
+        return <img key={id} alt={description} src={urls.regular} className='image' />
     })
-    return <div>{results}</div>
+    return <div className='container-div' >{results}</div>
 }
 
 export default ImageList;
