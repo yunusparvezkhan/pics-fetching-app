30daa3e58a8cc964911ab41450c3e6e9fda0d377
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-07 20:31:18 +0530

    Created responsive grid preview of the images, using js functions and css
    On this commit the app is now programmed to check each image's height and then a specific js method sets each image's style prop 'grid-row-end' as the appropriate number fetched from height followed by 'span'

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import "../styles/ImageList.css";
 
 export default class ImageCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { span: 0 }
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener("load", this.setSpans)
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const span = Math.ceil(height / 50 + 1);
+        this.setState({ span })
+    }
+
     render() {
         return (
-            <div>
-                <img alt={this.props.image.description}
+            <div style={{ gridRowEnd: `span ${this.state.span}` }} >
+                <img ref={this.imageRef}
+                    alt={this.props.image.description}
                     src={this.props.image.urls.regular}
                     className='image'
                 />
